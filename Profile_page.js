3b0a2922bbc6736955d0bd72072e19e3381df29c
Anil Pawale <120118798+anilpawale13@users.noreply.github.com>
2023-03-25 13:20:35 +0530

    Add files via upload

---
+++ Profile_page.js
@@ -0,0 +1,354 @@
+import React from "react";
+
+const Profile_pages =()=>{
+
+    return(
+        <div>
+        <section>
+       
+        <div id="pricing-table">
+        
+
+        <div class="priheading">
+
+
+        <nav  id="one" class="henavbar navbar-expand-sm" >
+       
+            <ol>
+                <h1 id="h1">Pricing</h1>
+            </ol>
+            <ul class="navbar-nav" id="navh">
+
+                <li class="nav-item-hp">
+                    <a class="nav-link " href="#">Home / </a>
+                </li>
+                <li class="nav-item-hp">
+                    <a class="nav-link" href="#">Pages / </a>
+                </li>
+                <li class="nav-item-hp">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+<div class="snip1276">
+    <div class="plan plan1" id="pl1">
+            
+                <div class="header">FREE<p>Lorem ipsum dolor</p></div>
+                
+                <div class="price"><div class="price1">0.00</div><p>/month</p></div>  
+                     
+                <ul>
+                    <li><b>Free</b>Ad Posting</li>
+                    <li><b>0</b> Featured Classes</li>
+                    <li><b>Custome </b>Reviews</li>
+              <li><b>24/7</b> Support</li>      
+                </ul>
+                <hr></hr>
+                <a class="signup" href="">Sign up</a>         
+            </div>
+            
+            <div class="plan plan2" id="pl2">
+            
+                <div class="header">PREMIUM<p>Lorem ipsum dolor</p></div>
+                
+             <div class="price"><div class="price1">19</div><p>/month</p></div> 
+                
+                <ul>
+                    <li><b>Free</b>Ad Posting</li>
+                    <li><b>20</b> Featured Classes</li>
+                    <li><b>Custome </b>Reviews</li>
+              <li><b>24/7</b> Support</li>      
+                </ul>
+                <hr></hr>
+                <a class="signup" href="">Sign up</a>            
+            </div>
+            
+            <div class="plan plan3" id="pl3">
+            
+               <div class="header">SILVER<p>Lorem ipsum dolor</p></div>
+               
+               <div class="price"><div class="price1">67</div><p>/month</p></div> 
+                
+                
+                  <ul>
+                    <li><b>Free</b>Ad Posting</li>
+                    <li><b>30</b> Featured Classes</li>
+                    <li><b>Custome </b>Reviews</li>
+              <li><b>24/7</b> Support</li>      
+                </ul>
+                <hr></hr>
+                <a class="signup" href="">Sign up</a> 
+                    
+            </div>
+            
+            <div class="plan plan4" id="pl4">
+            
+            <div class="header">GOLD<p>Lorem ipsum dolor</p></div>
+            
+               <div class="price"><div class="price1">67</div><p>/month</p></div> 
+            
+                <ul>
+                    <li><b>Free</b>Ad Posting</li>
+                    <li><b>40</b> Featured Classes</li>
+                    <li><b>Custome </b>Reviews</li>
+              <li><b>24/7</b> Support</li>      
+                </ul>
+                <hr></hr>
+                <a class="signup" href="">Sign up</a>        
+            </div>  
+        </div>
+      </div>
+        </section>
+
+        
+        
+        <div class="container">
+        <div class="row-fluid">
+
+
+            <h1>ANOTHER PRICING</h1>
+            <h4>Mauris ut cursus nunc. Morbi eleifend, ligula at consectetur vehicula</h4>
+
+        </div>
+        <div class="row-fluid">
+
+
+
+
+            <div class="span3 tiny ">
+                <div class="pricing-table-header-tiny">
+                    <h2>$65</h2>
+                    <h3>MONTHLY</h3>
+
+                    <div class="click">
+                        <h3>Business</h3>
+                    </div>
+
+                </div>
+                <div class="pricing-table-features">
+                    <p><strong>4</strong>Ads</p>
+                    <p><strong>30</strong>days</p>
+                    <p>Private Messages</p>
+                    <p>Urgent Ads</p>
+
+                </div>
+                <div class="pricing-table-signup-tiny">
+                    <p><a href="#">Choose plan</a></p>
+                </div>
+            </div>
+
+
+
+
+            <div class="span3 small">
+                <div class="pricing-table-header-small">
+                    <h2 class="pri2">$65</h2>
+                    <h3>MONTHLY</h3>
+
+                    <div class="click">
+                        <h3>Standard</h3>
+                    </div>
+
+                </div>
+                <div class="pricing-table-features">
+                    <p><strong>50</strong>Ads</p>
+                    <p><strong>60</strong>days</p>
+                    <p>Private Messages</p>
+                    <p> Urgent ads</p>
+
+                </div>
+                <div class="pricing-table-signup-small">
+                    <p><a href="#">Choose plan</a></p>
+                </div>
+            </div>
+
+
+            <div class="span3 medium">
+                <div class="pricing-table-header-medium">
+                    <h2>$100</h2>
+                    <h3>MONTHLY</h3>
+
+                    <div class="click">
+                        <h3>Premium</h3>
+                    </div>
+                </div>
+                <div class="pricing-table-features">
+                    <p><strong>100</strong>Ads</p>
+                    <p><strong>180</strong>days</p>
+                    <p>Private Messages</p>
+                    <p> Urgent ads</p>
+
+                </div>
+                <div class="pricing-table-signup-medium">
+                    <p><a href="#">Choose plan</a></p>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+        
+
+    <div class="topnav_a">
+  <div class="active_a">
+  <b>Subscribe To Our Onlinesletter</b>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p></div>
+  <div class="search-container">
+    <form action="/action_page.php">
+     <input type="text" placeholder="Enter your Email" name="search"/>
+      <button type="submit">Subscribe </button>
+    </form>
+  </div>
+</div>
+
+
+
+        
+
+<div class="pg-footer">
+    <footer class="footer">
+    
+      <div class="footer-content">
+        <div class="footer-content-column">
+         
+          <div class="footer-menu" id="jobcat">
+            <h2 class="footer-menu-name">JOB CATEGORIES</h2>
+            <ul id="menu-get-started" class="footer-menu-list">
+              <li class="menu-item menu-item-type-post_type menu-item-object-product">
+                <a href="#">Developement</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-product">
+                <a href="#">Designing</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-product">
+                <a href="#">Marketing</a>
+              </li>
+
+              <li class="menu-item menu-item-type-post_type menu-item-object-product">
+                <a href="#">Others</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-content-column">
+          <div class="footer-menu" id="jobt">
+            <h2 class="footer-menu-name">JOB TYPE</h2>
+            <ul id="menu-company" class="footer-menu-list">
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="#">Work from home</a>
+              </li>
+              <li class="menu-item menu-item-type-taxonomy menu-item-object-category">
+                <a href="#">Internship</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="#">Part time</a>
+              </li>
+
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+              <a href="#">Full time</a>
+            </li>
+            </ul>
+          </div>
+      
+        </div>
+        
+        <div class="footer-content-column">
+          <div class="footer-menu" id="resources">
+            <h2 class="footer-menu-name">RESOURCES</h2>
+            <ul id="menu-quick-links" class="footer-menu-list">
+              <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                <a target="_blank" rel="noopener noreferrer" href="#">Support</a>
+              </li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                <a target="_blank" rel="noopener noreferrer" href="#">FAQs</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="#">Terms of Service</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="#">Contact Details</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+    
+
+  
+   <div class="footer-content-column">
+          <div class="footer-menu" id="popular">
+            <h2 class="footer-menu-name">POPULAR ADS</h2>
+            <ul id="menu-quick-links" class="footer-menu-list">
+              <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                <a target="_blank" rel="noopener noreferrer" href="#">Educational college Ads</a>
+              </li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                <a target="_blank" rel="noopener noreferrer" href="#">Free Lancer for Web Developer</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="#">Searching for Developer</a>
+              </li>
+              <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="#">BPO Online In Bangalore</a>
+              </li>
+             
+            </ul>
+          </div>
+          
+        </div>
+  
+        <div class="footer-container">
+        <form action="/">
+         <input type="text" placeholder="Email" name="search"/>
+          <button type="submit" class="fbutton">Subscribe </button>
+        </form>
+    </div>
+          
+        </div>
+        <hr>
+        </hr>
+      <div class="footer-copyright">
+        <div class="footer-copyright-wrapper">
+          <p class="footer-copyright-text-p">Copyright © 2019  rejoin. Designed by
+            <a class="footer-copyright-link" href="#" target="_self">Spruko</a>
+             All rights reserved.
+          </p>
+        </div>
+        
+        
+        <hr>
+        </hr>
+        
+            <div class="footer-copyright">
+        <div class="footer-copyright-wrapper">
+          <p class="footer-copyright-text">
+            <a class="footer-copyright-link-l" href="#" target="_self">How it Works | About Us | Pricing | Ads Categories | Privacy Policy | Terms of Conditions | Blog | Contact Us | Pre</a>
+          </p>
+        </div>
+      </div>
+      </div>
+    </footer>
+  </div>
+
+ 
+</div>
+        
+        
+    )
+}
+
+export default Profile_pages
